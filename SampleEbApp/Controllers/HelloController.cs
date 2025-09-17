bca453aa321315c8f31f205e7a88e8b8b8b98d08
msjrafeerdeen <msjrafeerdeen@gmail.com>
2025-09-17 09:28:43 +0530

    sample application

---
+++ SampleEbApp/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleEbApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello from Elastic Beanstalk!" });
+        }
+    }
+}
